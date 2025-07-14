09900ea2448d78fe2b30adbfbd67c49160ee0d21
Slantall <slantallsstuff@gmail.com>
2025-07-14 16:36:14 -0700

    added tcp connection

--- main.go
+++ main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"net"
 )
 
-const inputFilePath = "messages.txt"
-
 func main() {
-	f, err := os.Open(inputFilePath)
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+		fmt.Println("Error creating listener: %w\n", err)
 	}
-	defer f.Close()
+	defer listener.Close()
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error with listener: %w\n", err)
+		}
+		fmt.Printf("Connection accepted: %v\n", connection)
+		str_ch := getLinesChannel(connection)
+		for s := range str_ch {
+			fmt.Printf("read: %s\n", s)
+		}
+		_, ok := <-str_ch
+		if !ok {
+			fmt.Println("Connection has been closed")
+		}
 
-	fmt.Printf("Reading data from %s\n", inputFilePath)
-	fmt.Println("=====================================")
-	str_ch := getLinesChannel(f)
-	for s := range str_ch {
-		fmt.Printf("read: %s\n", s)
 	}
 
 }
